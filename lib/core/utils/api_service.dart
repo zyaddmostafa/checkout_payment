f29e91ffa614fef9cefdade270ed0315ecf33387
zyad mostafa <zyadmo026@gmail.com>
2025-01-11 01:56:01 +0200

    create api service

---
+++ lib/core/utils/api_service.dart
@@ -0,0 +1,24 @@
+import 'package:dio/dio.dart';
+
+class ApiService {
+  final Dio dio = Dio();
+
+  Future<Response> post(
+      {required String body,
+      required String url,
+      required String token,
+      String? contentType}) async {
+    var response = await dio.post(
+      url,
+      data: body,
+      options: Options(
+        contentType: contentType,
+        headers: {
+          'Authorization': 'Bearer $token',
+        },
+      ),
+    );
+
+    return response;
+  }
+}
