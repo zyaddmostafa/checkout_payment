06da6055f8b0b2b340d514b0be13b61117f07cc3
zyad mostafa <zyadmo026@gmail.com>
2025-01-11 01:43:42 +0200

    create paymentintent

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:checkout_payment_ui/Features/checkout/presentation/views/my_cart_view.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() {
+  Stripe.publishableKey =
+      'pk_test_51QfqzNDRhw0UMOsFfP7u3bES8wShNt0vgMB3Ofseiyf0btonGjPVDWr95eUyXJuHcsXEXpGYQnzajrYxXiEsBSW700zyfd5Uu6';
   runApp(const CheckoutApp());
 }
 
@@ -16,3 +19,8 @@ class CheckoutApp extends StatelessWidget {
     );
   }
 }
+
+
+// paymentIntentObject =>  create payment intent (amount, currency) is required
+// init payment sheet (paymentsheetClientSecret)
+// present payment sheet ()
