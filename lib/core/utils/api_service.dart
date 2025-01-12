8cd83467608dd38d13566843bcee65e7fb308945
zyad mostafa <zyadmo026@gmail.com>
2025-01-12 04:40:23 +0200

    implement paymentintent

--- lib/core/utils/api_service.dart
+++ lib/core/utils/api_service.dart
@@ -1,10 +1,11 @@
+import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_model/payment_intent_input_model.dart';
 import 'package:dio/dio.dart';
 
 class ApiService {
   final Dio dio = Dio();
 
   Future<Response> post(
-      {required String body,
+      {required PaymentIntentInputModel body,
       required String url,
       required String token,
       String? contentType}) async {
--- lib/core/utils/stripe_service.dart
+++ lib/core/utils/stripe_service.dart
@@ -1,1 +1,19 @@
-class StripeService {}
+import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_model/payment_intent_input_model.dart';
+import 'package:checkout_payment_ui/Features/checkout/data/models/payment_intent_model/payment_intent_model.dart';
+import 'package:checkout_payment_ui/core/utils/api_keys.dart';
+import 'package:checkout_payment_ui/core/utils/api_service.dart';
+
+class StripeService {
+  final ApiService apiService = ApiService();
+  Future<PaymentIntentModel> createPaymentIntent(
+      PaymentIntentInputModel paymentIntentInputModel) async {
+    var response = await apiService.post(
+        body: paymentIntentInputModel.toJson(),
+        url: 'https://api.stripe.com/v1/payment_intents',
+        token: ApiKeys.secrtKey);
+
+    var paymentIntentModel = PaymentIntentModel.fromJson(response.data);
+
+    return paymentIntentModel;
+  }
+}
